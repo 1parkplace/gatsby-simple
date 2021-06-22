b630b6aca37c9bed24c26f9e5dad79f16260b79a
1parkplace <drewmeyer@1parkplace.com>
2021-06-22 13:35:07 -0700

    commit

---
+++ simple-hello/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Home() {
+  return <>
+          <div>Yes.</div>
+          <div>Last Updated: {new Date().toLocaleString()}</div>
+        </>
+}
